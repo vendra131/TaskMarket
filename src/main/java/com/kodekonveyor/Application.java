56d66e005661a1946cb87f638616a82f80cb6e2f
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-02-14 14:06:44 +0100

    Simplify configuration with Spring Boot

---
+++ src/main/java/com/kodekonveyor/Application.java
@@ -0,0 +1,25 @@
+package com.kodekonveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.kodekonveyor.annotations.ExcludeFromCodeCoverage;
+
+@SpringBootApplication
+@ExcludeFromCodeCoverage("empty")
+public class Application extends SpringBootServletInitializer {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(
+      final SpringApplicationBuilder builder
+  ) {
+    return builder.sources(Application.class);
+  }
+
+}
--- src/main/java/com/kodekonveyor/authentication/UserEntityRepository.java
+++ src/main/java/com/kodekonveyor/authentication/UserEntityRepository.java
@@ -3,7 +3,9 @@ package com.kodekonveyor.authentication;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserEntityRepository extends CrudRepository<UserEntity, Long> {
 
   List<UserEntity> findByLogin(String login);
