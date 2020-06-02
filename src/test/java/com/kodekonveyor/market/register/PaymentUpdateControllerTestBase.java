91e4cdcfcaeff6ff6b41acbc6cfa97d0ccebda76
Meenal2020 <61725187+Meenal2020@users.noreply.github.com>
2020-06-02 15:20:13 +0530

    Issue 126: PaymentUpdateController/Logging  (#172)

--- src/test/java/com/kodekonveyor/market/register/PaymentUpdateControllerTestBase.java
+++ src/test/java/com/kodekonveyor/market/register/PaymentUpdateControllerTestBase.java
@@ -4,9 +4,9 @@ package com.kodekonveyor.market.register;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.slf4j.Logger;
 
 import com.kodekonveyor.authentication.AuthenticatedUserService;
-import com.kodekonveyor.market.payment.PaymentUpdateController;
 import com.kodekonveyor.market.payment.TransferTypeEntityRepository;
 import com.kodekonveyor.market.payment.TransferTypeEntityRepositoryStubs;
 import com.kodekonveyor.market.project.ProjectEntityRepository;
@@ -28,6 +28,9 @@ public class PaymentUpdateControllerTestBase {
 
   @Mock
   TransferTypeEntityRepository transferTypeEntityRepository;
+
+  @Mock
+  Logger loggerService;
 
   @BeforeEach
   void setUp() {
--- src/test/java/com/kodekonveyor/market/register/RegisterTestData.java
+++ src/test/java/com/kodekonveyor/market/register/RegisterTestData.java
@@ -9,5 +9,8 @@ public class RegisterTestData {
   public static final String INVALID_TRANSFER_TYPE = "invalid transfer type";
   public static final String NO_MANAGER_ROLE_FOR_THE_PROJECT =
       "No manager role for the project";
-
+  public static final String NO_CAN_BE_PAID_ROLE =
+      "no can_be_paid role for user: ";
+  public static final String PAYMENT_UPDATED_SUCCESSFULLY =
+      "Payment updated successfully";
 }
